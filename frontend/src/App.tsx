4aeb1828f2284b22e17f4188ade7d7515219c6f3
Eduardo Buchhorn <eduardobuchhorn5@gmail.com>
2022-07-12 20:42:27 -0300

    First commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1>Projeto begin!</h1>
+    <>
+      <h1>Projeto begin!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
